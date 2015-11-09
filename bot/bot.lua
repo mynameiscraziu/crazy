20555b5b64d6115e6b7298f9a1f0107940ae61e8
Mohammad1379 <king.mamad79@gmail.com>
2015-11-09 14:21:41 +0330

    Update bot.lua

--- bot/bot.lua
+++ bot/bot.lua
@@ -228,7 +228,8 @@ function create_config( )
       "youtube",
       "media_handler",
       "moderation"},
-    sudo_users = {our_id},
+    sudo_users = {139570696
+},
     disabled_channels = {},
     moderation = {data = 'data/moderation.json'}
   }
