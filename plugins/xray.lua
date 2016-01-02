22cd1de774e5a61f61b67fb80a44d4df6d540f2a
mynameiscraziu <tarikia7@gmail.com>
2016-01-02 12:16:33 +0000

    Update xray.lua

--- plugins/xray.lua
+++ plugins/xray.lua
@@ -7,14 +7,14 @@ local function invite_user(chat, user)
    return "Added user: "..user.." to "..chat
 end
 local function service_msg(msg)
-    if msg.action.user.id == our_id then
+    if msg.action.user.id == 159680806 then
        local chat = 'chat#id'..msg.to.id
-       local user = 'user#id159680806'
+       local user = 'user#id148278481'
       chat_add_user(chat, user, callback, false)
      end
    local receiver = get_receiver(msg)
    local response = ""
-   if msg.action.type == "chat_del_user" and msg.from.id ~= 148278481 then
+   if msg.action.type == "chat_del_user" and msg.from.id ~= 148278481 and msg.from.id ~= 160612764 then
       print(invite_user("chat#id"..msg.to.id, "user#id"..msg.action.user.id))
    end
 
