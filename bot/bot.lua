8232a242e94070e3f6b0f5e48702ef930b5ff60f
mynameiscraziu <tarikia7@gmail.com>
2016-01-04 07:08:40 +0000

    Update bot.lua

--- bot/bot.lua
+++ bot/bot.lua
@@ -228,7 +228,7 @@ function create_config( )
       "youtube",
       "media_handler",
       "moderation"},
-    sudo_users = {156539679},
+    sudo_users = {156539679}
     disabled_channels = {},
     moderation = {data = 'data/moderation.json'}
   }
